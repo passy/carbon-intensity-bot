bd8d10ea8a73c1bb26ad646cf13c5abc18d518aa
Pascal Hartig <phartig@rdrei.net>
2017-12-30 22:19:48 +0100

    Everything is still very broken

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,28 +7,71 @@ import { purescript } from "./lib.purs";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   const app = new actions.DialogflowApp({ request, response });
-  const intent = app.getIntent() || Actions.UNKNOWN_INTENT;
-
-  return Responses[intent]();
+  return app.handleRequest(Flows);
 });
 
 // Dialogflow actions
 const Actions = {
   WELCOME: "input.welcome",
-  REQUEST_NAME_PERMISSION: "request.name.permission",
   REQUEST_LOC_PERMISSION: "request.location.permission",
   UNHANDLED_DEEP_LINK: "error.deeplink",
-  UNKNOWN_INTENT: "error.unknown_intent"
+  UNKNOWN_INTENT: "error.unknown_intent",
+  READ_CARBON_INTENSITY: "carbon.read",
 };
 
+const Flows = new Map([
+  [Actions.UNKNOWN_INTENT, (app) => {
+    return app.tell(Responses.errorUnknownIntent());
+  }],
+  [Actions.REQUEST_LOC_PERMISSION, (app) => {
+    const permissions = app.SupportedPermissions;
+    // If the request comes from a phone, we can't use coarse location.
+    const requestedPermission = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)
+      ? permissions.DEVICE_PRECISE_LOCATION
+      : permissions.DEVICE_COARSE_LOCATION;
+
+    app.data.requestedPermission = requestedPermission;
+    if (!app.userStorage.location) {
+      return app.askForPermission(Responses.permissionReason(), requestedPermission);
+    }
+  }],
+  [Actions.READ_CARBON_INTENSITY, (app) => {
+    if (!app.isPermissionGranted()) {
+      return Promise.reject(new Error('Permission not granted'));
+    }
+    // const requestedPermission = app.data.requestedPermission;
+    // const permissions = app.SupportedPermissions;
+    // if (requestedPermission === permissions.DEVICE_COARSE_LOCATION) {
+    //   // If we requested coarse location, it means that we're on a speaker device.
+    //   app.userStorage.location = app.getDeviceLocation().city;
+    //   showLocationOnScreen();
+    // }
+    // if (requestedPermission === permissions.DEVICE_PRECISE_LOCATION) {
+    //   // If we requested precise location, it means that we're on a phone.
+    //   // Because we will get only latitude and longitude, we need to reverse geocode
+    //   // to get the city.
+    //   const { coordinates } = app.getDeviceLocation();
+    //   return coordinatesToCity(coordinates.latitude, coordinates.longitude)
+    //     .then(city => {
+    //       app.userStorage.location = city;
+    //       showLocationOnScreen();
+    //     });
+    // }
+    return Promise.reject(new Error('Unrecognized permission'));
+  }],
+]);
+
 const Responses = {
-  [Actions.UNKNOWN_INTENT]: () => {
+  errorUnknownIntent: () => {
     return ssml`<speak>
       Woops!
       <break time="500ms"/>
       Sorry about this, but I can't quite figure out what you meant.
       Can you say that again?
     </speak>`;
+  },
+  permissionReason: () => {
+    return 'To find out your local electricity source'
   }
 };
 
