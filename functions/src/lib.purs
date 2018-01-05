d547a162a64f6c005548d58df0dfda9c96099cae
Pascal Hartig <phartig@rdrei.net>
2018-01-05 21:24:51 +0100

    Add functions for requesting country code based data

--- functions/src/lib.purs
+++ functions/src/lib.purs
@@ -5,13 +5,16 @@ import Prelude
 import Control.Monad.Aff (Aff, throwError, error)
 import Control.Monad.Eff (Eff)
 import Control.Promise as Promise
+import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.?))
 import Data.Either (Either(Right, Left))
-import Data.Function.Uncurried (Fn3, mkFn3)
+import Data.Function.Uncurried (Fn2, Fn3, mkFn2, mkFn3)
 import Network.HTTP.Affjax (get, URL, Affjax, AJAX)
 import Network.HTTP.Affjax.Response (class Respondable)
+import Network.HTTP.StatusCode (StatusCode(..))
 
 newtype ApiToken = ApiToken URL
+newtype CountryCode = CountryCode String
 
 baseUrl :: String
 baseUrl = "https://api.co2signal.com/v1/"
@@ -33,22 +36,45 @@ instance decodeJsonCo2Response :: DecodeJson Co2Response where
     fossilFuelPercentage <- data' .? "fossilFuelPercentage"
     pure $ Co2Response { countryCode, carbonIntensity, datetime, fossilFuelPercentage }
 
--- TODO: Tag lat/lon
-requestCo2LatLonAff :: forall e a. Respondable a => ApiToken -> Number -> Number -> Affjax e a
-requestCo2LatLonAff (ApiToken token) lat lon =
+-- TODO: Type lat/lon
+requestCo2LatLonAff :: forall e a. Respondable a => Number -> Number -> ApiToken -> Affjax e a
+requestCo2LatLonAff lat lon (ApiToken token) =
     get $ baseUrl <> "latest?lat=" <> show lat <> "&lon=" <> show lon <>
-                                    "&api-token" <> token
+                                    "&auth-token=" <> token
 
-getCo2LatLonAff :: forall eff. ApiToken -> Number -> Number -> Aff (ajax :: AJAX | eff) (Either String Co2Response)
-getCo2LatLonAff token lat lon = do
-  { response } <- requestCo2LatLonAff token lat lon
+requestCo2CountryAff :: forall e a. Respondable a => CountryCode -> ApiToken -> Affjax e a
+requestCo2CountryAff (CountryCode code) (ApiToken token) =
+    get $ baseUrl <> "latest?countryCode=" <> code <> "&auth-token=" <> token
+
+getCo2Aff
+    :: forall eff
+    .  Affjax eff Json
+    -> Aff (ajax :: AJAX | eff) (Either String Co2Response)
+getCo2Aff req = do
+  { status, response } <- req
+  when (status /= StatusCode 200) $ throwError $ error $ "Response code " <> show status <> " /= 200"
   pure $ decodeJson response
 
-requestCo2LatLon_ :: forall eff. ApiToken -> Number -> Number -> Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response)
-requestCo2LatLon_ token lat lon = Promise.fromAff $ do
-    getCo2LatLonAff token lat lon >>= case _ of
+requestCo2
+    :: forall eff a
+    .  (ApiToken -> Affjax eff Json)
+    -> ApiToken
+    -> Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response)
+requestCo2 fn token = Promise.fromAff $ do
+    getCo2Aff (fn token) >>= case _ of
         Left e -> throwError $ error e
         Right res -> pure res
 
+requestCo2LatLon_ :: forall eff. ApiToken -> Number -> Number -> (Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response))
+requestCo2LatLon_ token lat lon =
+    requestCo2 (requestCo2LatLonAff lat lon) token
+
 requestCo2LatLon :: forall eff. Fn3 ApiToken Number Number (Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response))
 requestCo2LatLon = mkFn3 requestCo2LatLon_
+
+requestCo2Country_ :: forall eff. ApiToken -> CountryCode -> (Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response))
+requestCo2Country_ token countryCode =
+    requestCo2 (requestCo2CountryAff countryCode) token
+
+requestCo2Country :: forall eff. Fn2 ApiToken CountryCode (Eff (ajax :: AJAX | eff) (Promise.Promise Co2Response))
+requestCo2Country = mkFn2 requestCo2Country_
