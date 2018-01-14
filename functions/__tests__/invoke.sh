d6be62621c74b0fbafd803740d3ef0b91c9e9dd7
Pascal Hartig <phartig@rdrei.net>
2018-01-14 15:12:07 +0000

    Add an example request

---
+++ functions/__tests__/invoke.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Obviously, make this part of the real integration test suite.
+curl -vv 'http://localhost:5000/carbon-intensity/us-central1/webhook' -H 'Content-Type: application/json' --data @requests/carbon_latlon.json
