842a7493e8d8bd07111c6efdde0ad8488a80177a
Pascal Hartig <phartig@rdrei.net>
2018-01-14 15:22:42 +0000

    Add coarse request fixture

--- functions/__tests__/invoke.sh
+++ functions/__tests__/invoke.sh
@@ -2,3 +2,6 @@
 
 # Obviously, make this part of the real integration test suite.
 curl -vv 'http://localhost:5000/carbon-intensity/us-central1/webhook' -H 'Content-Type: application/json' --data @requests/carbon_latlon.json
+curl -vv 'http://localhost:5000/carbon-intensity/us-central1/webhook' -H 'Content-Type: application/json' --data @requests/carbon_zip.json
+
+# vim: tw=0
