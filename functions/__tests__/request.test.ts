cda2d1c5d1399b80f66f574193f505943b3765ca
Pascal Hartig <phartig@rdrei.net>
2018-01-28 18:47:41 +0000

    Formatting

--- functions/__tests__/request.test.ts
+++ functions/__tests__/request.test.ts
@@ -1,26 +1,38 @@
-import * as triggers from '../lib/index.js';
-import * as fs from 'fs';
-import * as path from 'path';
-import {MockRequest, MockResponse, headerV2} from 'actions-on-google/test/utils/mocking';
-import * as functions from 'firebase-functions';
+import * as triggers from "../lib/index.js";
+import * as fs from "fs";
+import * as path from "path";
+import {
+  MockRequest,
+  MockResponse,
+  headerV2
+} from "actions-on-google/test/utils/mocking";
+import * as functions from "firebase-functions";
 
 // Manually mock that static global.
 functions.config = () => {
-  return JSON.parse(fs.readFileSync(path.join(__dirname, '..', '.runtimeconfig.json'), { encoding: 'utf-8'}));
+  return JSON.parse(
+    fs.readFileSync(path.join(__dirname, "..", ".runtimeconfig.json"), {
+      encoding: "utf-8"
+    })
+  );
 };
 
 const loadFixture = name => {
-  const body = JSON.parse(fs.readFileSync(path.join(__dirname, 'requests', name + '.json'), {encoding: 'utf-8'}));
-  return new MockRequest(headerV2, body)
+  const body = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "requests", name + ".json"), {
+      encoding: "utf-8"
+    })
+  );
+  return new MockRequest(headerV2, body);
 };
 
-it('sends a response', () => {
+it("sends a response", () => {
   expect.assertions(1);
-  const req = loadFixture('carbon_zip');
+  const req = loadFixture("carbon_zip");
 
   const resp = new MockResponse();
   triggers.webhook(req, resp);
 
   expect(resp.statusCode).toBe(200);
-  expect(resp.body).toBe('hello world');
+  expect(resp.body).toBe("hello world");
 });
