8a9f078ab9d250b5338225c111a57fa4c03813fa
Pascal Hartig <phartig@rdrei.net>
2018-04-04 19:29:31 +0200

    Set up an incorrect but working test

--- functions/__tests__/decode.test.ts
+++ functions/__tests__/decode.test.ts
@@ -3,8 +3,6 @@ import * as fs from "fs";
 import * as path from "path";
 
 it('parses my partial JSON', () => {
-  expect.assertions(2);
-
-  const r = lib.decodeCo2Response("{}");
-  expect(r).toBeNull();
+  const r = lib.decodeCo2Response(JSON.parse('{"_disclaimer":"This data is the exclusive property of Tomorrow and/or related parties. If you\'re in doubt about your rights to use this data, please contact hello@tmrow.com","status":"ok","countryCode":"US","data":{"fossilFuelPercentage":null},"units":{"carbonIntensity":"gCO2eq/kWh"}}'));
+  expect(r).not.toBeNull();
 });
