77da7e827fbae3a1f7df9afe33d97cc0ffe3ea41
Pascal Hartig <phartig@rdrei.net>
2018-12-30 21:21:38 +0100

    Upgrade webpack config

--- functions/webpack.config.js
+++ functions/webpack.config.js
@@ -1,39 +1,47 @@
-'use strict';
+"use strict";
 
-const nodeExternals = require('webpack-node-externals');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const nodeExternals = require("webpack-node-externals");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    target: 'node',
-    output: {
-        filename: 'lib/index.js',
-        libraryTarget: 'this',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                options: {
-                    transpileOnly: true,
-                },
-            },
-            {
-                test: /\.purs$/,
-                loader: 'purs-loader',
-                options: {
-                    psc: 'psa',
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [ '.ts', '.tsx', '.js', '.purs' ],
-    },
-    externals: [nodeExternals()],
-    plugins: [
-        // new ForkTsCheckerWebpackPlugin()
-    ],
+  entry: "./src/index.ts",
+  mode: "production",
+  target: "node",
+  output: {
+    filename: "lib/index.js",
+    libraryTarget: "this"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.purs$/,
+        use: [
+          {
+            loader: "purs-loader",
+            options: {
+              psc: "psa"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".purs"]
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    // new ForkTsCheckerWebpackPlugin()
+  ]
 };
